c2629b8af787e7693ece3675808be3d350fbc16f
lin <citylcs@gmail.com>
2017-09-19 21:56:41 +0800

    构建完成

---
+++ build/main.js
@@ -0,0 +1,6 @@
+
+ReactDOM.render(React.createElement(
+    'h1',
+    null,
+    'Hello, world!\u554A\u554A\u554A\u554A\u554A\u554A\u554A\u554A\u554A\u554A\u554A'
+), document.getElementById('app'));
