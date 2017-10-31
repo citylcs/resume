35b5763a976752ef57b882ac4f40928e45886ea9
lin <citylcs@gmail.com>
2017-10-31 22:12:08 +0800

    增加更多条目及学习经历项

--- src/resume.js
+++ src/resume.js
@@ -154,6 +154,29 @@ class Experience extends React.Component {
     }
 }
 
+class SkillOverview extends React.Component {
+    render() {
+        let skillOver = this.props.skillOver.map((item, index) => {
+            if (item.name) {
+                return <span key={index}>{item.name}</span>
+            }
+        });
+        let skillOverName = this.props.skillOver.map((item, index) => {
+            if (item.title) {
+                return <h2 id="skillOverTitle" key={index}>{item.title}</h2>
+            }
+        });
+        return (
+            <div id="skillOver">
+                {skillOverName}
+                <div id="overWrap">
+                    {skillOver}
+                </div>
+            </div>
+        )
+    }
+}
+
 class SkillsTitle extends React.Component {
     render() {
         let htmlTitle = this.props.html.map((item) => {
@@ -326,6 +349,80 @@ class Js extends React.Component {
     }
 }
 
+class Frameworks extends React.Component {
+    render() {
+        let description = this.props.frameworks.map((item, index) => {
+            if (item.description) {
+                return <li key={index}>{item.description}</li>
+            }
+        });
+        let title = this.props.frameworks.map((item, index) => {
+            if (item.title) {
+                return <h2 className="frameworks" key={index}>{item.title}</h2>
+            }
+        });
+        return(
+            <div id="frameworks">
+                {title}
+                <div className="target_border3">
+                    <div className="target_dote3"> </div>
+                </div>
+                <ul className="frameworksDes">
+                    {description}
+                </ul>
+            </div>
+        )
+    }
+}
+
+class LearningExperience extends React.Component {
+    render() {
+        let title = this.props.learningExperience.map((item, index) => {
+            if (item.title) {
+                return <h2 id="learningTitle" key={index}>{item.title}</h2>
+            }
+        });
+        let learningTime1 = this.props.learningExperience.map((item, index) => {
+            if (item.time1) {
+                return <p id="time1" key={index}>{item.time1}</p>
+            }
+        });
+        let learningDes1 = this.props.learningExperience.map((item, index) => {
+            if (item.description1) {
+                return <li key={index}>{item.description1}</li>
+            }
+        });
+        let learningTime2 = this.props.learningExperience.map((item, index) => {
+            if (item.time2) {
+                return <p id="time2" key={index}>{item.time2}</p>
+            }
+        });
+        let learningDes2 = this.props.learningExperience.map((item, index) => {
+            if (item.description2) {
+                return <li key={index}>{item.description2}</li>
+            }
+        });
+        return (
+            <div id="learningExperience">
+                {title}
+                <div className="target_border4">
+                    <div className="target_dote4"> </div>
+                </div>
+                <ul className="leaningExperience">
+                    <li className="mooc">
+                        {learningTime1}
+                        <ul className="learningDes1">{learningDes1}</ul>
+                    </li>
+                    <li className="netEasy">
+                        {learningTime2}
+                        <ul className="learningDes2">{learningDes2}</ul>
+                    </li>
+                </ul>
+            </div>
+        )
+    }
+}
+
 class ProjectTitle extends React.Component {
     render() {
         let title = this.props.project.map((item) => {
@@ -363,8 +460,8 @@ class Project extends React.Component {
         return (
             <div>
                 <ProjectTitle project={this.props.project} />
-                <div className="target_border3">
-                    <div className="target_dote3"> </div>
+                <div className="target_border5">
+                    <div className="target_dote5"> </div>
                 </div>
                 <ProjectList project={this.props.project} />
             </div>
@@ -380,11 +477,31 @@ const ITEMS = [
 ];
 const EXP = [
     {ExpTitle: 'Experience'},
-    {ExpCompanyName: '北京拓思德科技有限公司 (电子教育笔)'},
-    {description: '坐标测量员(实习)'},
+    {ExpCompanyName: '北京拓思德科技有限公司'},
+    {description: '坐标测量员(软件测试)'},
     {description: 'Jul 2014 - Sep 2014'},
     {expList: '负责该公司研发的一款电子教育笔以及与苹果公司合作的Apple Pencil的坐标测量，精确定位图像出现在区域内相应位置'},
     {expList: '负责该公司产品的包装和市场宣传'},
+];
+
+const SKILLOVER = [
+    {title: 'Skills Overview'},
+    {name: 'HTML(5)'},
+    {name: 'CSS(3)'},
+    {name: 'JavaScript'},
+    {name: 'DOM'},
+    {name: 'W3C标准'},
+    {name: 'ES6'},
+    {name: '语义化'},
+    {name: '响应式布局'},
+    {name: 'Git'},
+    {name: 'Ajax'},
+    {name: 'JQuery'},
+    {name: 'React'},
+    {name: 'bootstrap'},
+    {name: 'SASS'},
+    {name: 'Gulp'},
+    {name: 'Webpack'},
 ];
 
 const HTML1 = [
@@ -400,31 +517,50 @@ const CSS = [
     {name: 'CSS'},
     {description: 'CSS3'},
     {description: 'transform'},
-    {description: 'animate'},
-    {description: 'bootstrap'},
     {list: '熟练使用css，擅长div + css的布局，能够写出兼容常见浏览器的代码'},
     {list: '熟悉盒模型，以及各类布局的使用方法'},
     {list: '熟悉css3新增属性的使用，border-radius、transform、animation等'},
     {list: '熟练使用响应式布局适配各设备的屏幕尺寸'},
     {list: '熟练使用SASS预处理器'},
-    {list: '熟悉bootstrap、animation.css等css框架的使用'},
 ];
 
 const JS = [
     {name: 'JavaScript'},
-    {description: 'jQuery'},
-    {description: 'React'},
     {description: 'ES6'},
     {description: 'DOM'},
     {list: 'JavaScript基础牢固，熟悉DOM、event、ajax，能够编写出基本的JavaScript原生代码'},
     {list: '熟悉ES6语法，个人项目中已全面使用ES6进行开发'},
-    {list: '熟悉jQuery api，能够编写出最基本的动态交互效果，并擅长用其封装日常的开发组件'},
-    {list: '熟悉React、underscore等框架，对前端组件化有深入了解'},
-    {list: '熟练使用Git版本控制工具'},
-    {list: '熟练使用Gulp、webpack等前端自动化构建工具'},
     {list: '熟练使用chrome developer tools调试工具来调试代码'},
     {list: '熟练使用intellij idea、Sublime Text3等编辑器进行前端页面手工式的开发'},
     {list: '熟练使用Google、Stack Overflow等网站解决问题'},
+];
+
+const FRAMEWORKS = [
+    {title: 'Frameworks & Tools'},
+    {description: '熟悉jQuery api，能够编写出最基本的动态交互效果，并擅长用其封装日常的开发组件'},
+    {description: '熟悉React、underscore等框架，对前端组件化有深入了解'},
+    {description: '熟悉bootstrap、animation.css等css框架的使用'},
+    {description: '熟练使用Git版本控制工具'},
+    {description: '熟练使用Gulp、webpack等前端自动化构建工具'},
+];
+
+const LEARNING = [
+    {title: 'Learning Experience'},
+    {time1: 'Mar 2016 - Jun - 2016 慕课网'},
+    {time2:'Aug 2016 - Dec 2016 网易云课堂'},
+    {description1: 'HTML + CSS基础课程'},
+    {description1: 'CSS网页布局'},
+    {description1: '网页简单布局之结构与表现原则'},
+    {description1: '导航条菜单制作'},
+    {description1: 'JavaScript 入门 + 进阶'},
+    {description1: 'DOM事件'},
+    {description1: 'JQuery样式'},
+    {description1: 'JQuery DOM'},
+    {description2: '页面制作 HTML + CSS + 开发调试工具'},
+    {description2: 'JavaScript 基础篇 + 进阶篇'},
+    {description2: 'DOM编程艺术'},
+    {description2: '页面架构'},
+    {description2: '产品前端架构'},
 ];
 
 const PROJECT = [
@@ -432,7 +568,8 @@ const PROJECT = [
     {list: 'Victor-series-page' ,description: '网络加速服务类主页', type: 'HTML',light: '#e34c26', url: 'https://github.com/citylcs/Victor-series-page'},
     {list: 'toDoList', description: '原生JavaScript开发的todolist应用', type: 'JavaScript', light: '#f1e054', url: 'https://github.com/citylcs/toDoList'},
     {list: 'Calculation', description: '一个简单的、原生JavaScript制作的计算器', type: 'CSS', light: '#563d7c', url: 'https://github.com/citylcs/Calculation'},
-    {list: 'React-TODO', description: 'React.js框架下开发的带过滤功能的todolist应用', type: 'JavaScript', light: '#f1e054', url: 'https://github.com/citylcs/React-TODO'},
+    {list: 'React Demo', description: 'React.js框架下开发的带过滤功能的todolist应用', type: 'JavaScript', light: '#f1e054', url: 'https://github.com/citylcs/React-TODO'},
+    {list: 'resume', description: 'React + SASS + Gulp + Webpack开发的个人简历', type: 'JavaScript', light: '#f1e054', url: 'https://github.com/citylcs/resume'},
 ];
 
 function Resume(){
@@ -448,10 +585,13 @@ function Resume(){
                         [
                             <div key="1"><Overview items={ITEMS} /></div>,
                             <div key="2"><Experience exp={EXP} /></div>,
-                            <div key="3"><HTML html={HTML1} /></div>,
-                            <div key="4"><Css css={CSS} /></div>,
-                            <div key="5"><Js js={JS} /></div>,
-                            <div key="6"><Project project={PROJECT} /></div>,
+                            <div key="3"><SkillOverview skillOver={SKILLOVER} /></div>,
+                            <div key="4"><HTML html={HTML1} /></div>,
+                            <div key="5"><Css css={CSS} /></div>,
+                            <div key="6"><Js js={JS} /></div>,
+                            <div key="7"><Frameworks frameworks={FRAMEWORKS} /></div>,
+                            <div key="8"><LearningExperience learningExperience={LEARNING} /></div>,
+                            <div key="9"><Project project={PROJECT} /></div>,
                         ]
                     }
                 </QueueAnim>
